599a47eab86dc75a14fcc96e815865ad6083a411
Martin Mackintosh <martindmackintosh@outlook.com>
2025-02-08 11:18:14 +0000

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,7 @@
+module mdmackint/kfarm
+
+go 1.23.4
+
+require golang.org/x/net v0.34.0
+
+require github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
--- kfarm.go
+++ kfarm.go
@@ -1,13 +1,49 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"math/rand"
+	"net/http"
 	"strconv"
 	"time"
-	"math/rand"
+    "github.com/common-nighthawk/go-figure"
 )
+
+var stage uint8 = 1
+
+func colour(sc int) string {
+	switch sc {
+	case 200:
+		return "green"
+	case 500:
+		return "red"
+	default:
+		return "purple"
+	}
+}
+
+func progress() {
+	for range time.Tick(500 * time.Millisecond) {
+		switch stage {
+		case 1:
+			fmt.Print("\r-")
+		case 2:
+			fmt.Print("\r\\")
+		case 3:
+			fmt.Print("\r|")
+		case 4:
+			fmt.Print("\r/")
+		}
+		if stage != 4 {
+			stage++
+		} else {
+			stage = 1
+		}
+	}
+}
 func main() {
+	fmt.Println("Good luck!")
+	go progress()
 	for i := 100000; i < 1000000; i++ {
 		pin := rand.Intn(1000000)
 		now := time.Now()
@@ -17,7 +53,9 @@ func main() {
 			panic("Error thrown during game pin checking.")
 		}
 		if x.StatusCode != 404 {
-			fmt.Println("Game PIN: " + strconv.Itoa(pin) + " - " + x.Status)
+			fmt.Print("\r")
+			ascii := figure.NewColorFigure(strconv.Itoa(pin),"",colour(x.StatusCode),true)
+            ascii.Print()
 		}
 	}
 }
